c26576bcf82c022aa7a3653afec3e7b92afce92d
jmint8 <jmitchelmeier@gmail.com>
2026-03-09 01:02:20 -0400

    added the loggers

---
+++ src/main/java/tournament/MoveLogger.java
@@ -0,0 +1,37 @@
+package tournament;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MoveLogger implements GameObserver {
+	private String filePath;
+	private FileWriter writer;
+	
+	public MoveLogger(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	@Override
+	public void onMoveMade(MoveEvent event) {
+		try (FileWriter fileW = new FileWriter(filePath, true)) {
+            RoundResult r = event.getRound();
+            fileW.write("Round " + r.getRoundNumber() + ": P1: " + r.getActionP1().getLabel() + 
+                     " (Score: " + r.getScoreP1() + "), P2: " + r.getActionP2().getLabel() + 
+                     " (Score: " + r.getScoreP2() + ")\n");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
+
+	@Override
+	public void onTournamentEnd(TournamentResult result) {
+		
+
+	}
+
+	@Override
+	public void onGameEnd(GameResult result) {
+
+	}
+
+}
