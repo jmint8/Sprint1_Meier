f11c558b3a688a105e93e57a6deff5fdec884bf9
jmint8 <jmitchelmeier@gmail.com>
2026-03-16 08:44:18 -0400

    added the logger test file, neeed to write

---
+++ src/test/java/SITS/LoggerTests.java
@@ -0,0 +1,20 @@
+package SITS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoggerTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void test() {
+		//TODO
+	}
+
+}
