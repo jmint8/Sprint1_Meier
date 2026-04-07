1330a71fe55489a0319fe8088d96baba8ca7d8d8
jmint8 <jmitchelmeier@gmail.com>
2026-04-06 20:52:23 -0400

    added the participant controller and the GameHistoryDTO because they looked small, they should be in good shape

--- src/main/java/SITS/Remote/Client/ParticipantController.java
+++ src/main/java/SITS/Remote/Client/ParticipantController.java
@@ -1,5 +1,37 @@
 package SITS.Remote.Client;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import SITS.Actions.Participant;
+import SITS.Game.GameHistory;
+import SITS.Remote.Network.dto.GameHistoryDTO;
+
+
 public class ParticipantController {
+	
+	private Participant participant;
+	
+	public ParticipantController(Participant participant)
+	{
+		this.participant = participant;
+	}
+	
+	@GetMapping("/name")
+	public String getName() { 
+		return participant.getName();
+	}
+	
+	@GetMapping("/action")
+	public String getAction(@RequestBody GameHistoryDTO dto) {
+		GameHistory history = dto.toGameHistory();
+		return participant.chooseAction(history).getLabel();
+	}
+		
+	@PostMapping("/reset")
+	public void reset() {
+		participant.reset();
+	}		
 
 }
--- src/main/java/SITS/Remote/Network/dto/GameHistoryDTO.java
+++ src/main/java/SITS/Remote/Network/dto/GameHistoryDTO.java
@@ -1,5 +1,54 @@
 package SITS.Remote.Network.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import SITS.Game.GameHistory;
+import SITS.Game.RoundResult;
+import SITS.Remote.Network.StringAction;
+
 public class GameHistoryDTO {
+	
+	public String nameP1;
+	public String nameP2;
+	public List<RoundResultDTO> rounds;
+	
+	public GameHistoryDTO(String nameP1, String nameP2, List<RoundResultDTO> rounds) {
+		this.nameP1 = nameP1;
+		this.nameP2 = nameP2;
+		this.rounds = rounds;
+	}
+	
+	public GameHistory toGameHistory() {
+		GameHistory history = new GameHistory(nameP1, nameP2);
+		int numRounds = 1;
+		for(RoundResultDTO round: rounds) {
+			history.getRounds().add(new RoundResult(
+					new StringAction(round.actionP1),
+					new StringAction(round.actionP2),
+					round.payoffP1,
+					round.payoffP2,
+					numRounds++
+							));
+		}
+		
+		return history;
+	}
+	
+	
+	public static GameHistoryDTO fromGameHistory(GameHistory history) {
+		List<RoundResultDTO> dtos = new ArrayList<>();
+
+		for (RoundResult round : history.getRounds()) { 
+			 
+			dtos.add(new RoundResultDTO(
+					round.getActionP1().getLabel(), 
+					round.getActionP2().getLabel(), 
+					round.getScoreP1(),
+					round.getScoreP2())); 
+			} 
+		return new GameHistoryDTO(history.getP1Name(), history.getP2Name(), dtos);
+	}
+	
 
 }
