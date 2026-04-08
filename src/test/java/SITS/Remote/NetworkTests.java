b4a3b2431fa1e9ea27a74a41f908e9e66d921d4b
jmint8 <jmitchelmeier@gmail.com>
2026-04-08 08:59:11 -0400

    added the Junit Test cases for the Server, Network, and DTO

---
+++ src/test/java/SITS/Remote/NetworkTests.java
@@ -0,0 +1,22 @@
+package SITS.Remote;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NetworkTests {
+
+	@BeforeEach
+	void setUp() throws Exception 
+	{
+		
+	}
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/SITS/Remote/ServerTests.java
@@ -0,0 +1,22 @@
+package SITS.Remote;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ServerTests {
+
+	@BeforeEach
+	void setUp() throws Exception 
+	{
+		
+	}
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
