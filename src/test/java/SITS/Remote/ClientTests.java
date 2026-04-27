8cba3e38b7bc565b22e046a80a9ddc148de5c999
jmint8 <jmitchelmeier@gmail.com>
2026-04-27 08:13:17 -0400

    added the regitration test endpoint to test if registration works.

--- src/test/java/SITS/Remote/ClientTests.java
+++ src/test/java/SITS/Remote/ClientTests.java
@@ -3,6 +3,7 @@ package SITS.Remote;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,11 +16,17 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import SITS.Remote.Client.ClientApp;
 import SITS.Remote.Client.TournamentServerClient;
 import SITS.Remote.Network.dto.GameHistoryDTO;
+import SITS.Remote.Network.dto.RegistrationRequest;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -54,6 +61,33 @@ class ClientTests {
 				}
 			};
 		}
+		// a dummy test controller hosted on clientApp for
+		//TounramentServerClient testing
+		@RestController
+		static class testServerController
+		{
+			//this is for testing the registration. verrifying that the regirster function works 
+			public List<String> regiPlayers = new ArrayList<>();
+			
+			@GetMapping("/tournaments")
+			public List<String> testGetTournaments(){
+				List<String> list = new ArrayList<>();
+				list.add("ipd-1");
+				return list;
+			}
+			@PostMapping("/register/{id}")
+			public ResponseEntity<String> dummyRegister(@PathVariable String id, @RequestBody RegistrationRequest req) {
+				regiPlayers.add(req.name);// this adds the name to the array
+				return ResponseEntity.ok("registered " + req.name);
+			}
+			//this is literally only for verifying the register method. 
+			@GetMapping("/registered")
+			public List<String> getRegistered(){
+				 
+				return regiPlayers;
+			}
+			
+		}
 	}
 	
 	//name endpoint
@@ -73,6 +107,7 @@ class ClientTests {
 		assertEquals("COOPERATE", response.getBody());
 	}
 
+	//reset endpoint
 	@Test
 	void resetTest() {
 		String url = "http://localhost:"+port+"/reset";
@@ -80,8 +115,40 @@ class ClientTests {
 		assertTrue(response.getStatusCode().is2xxSuccessful());
 	}
 
-
-
+	//we need to test the tournamentServerContoller here too. 
+	
+	//testing getServerUrl()
+	@Test
+	void testTournamentgeturl() {
+		String url = "http://localhost:" + port;
+		TournamentServerClient client = new TournamentServerClient(url);
+		assertEquals(url,client.getServer_url());
+		assertNotNull(client.getRestTemplate());	
+	}
+	
+	
+	@Test
+	void listTournametsTest() {
+		TournamentServerClient client = new TournamentServerClient("http://localhost:" + port);
+		
+		List<?> tourna = client.listTournaments();
+		assertNotNull(tourna);
+		assertEquals("ipd-1", tourna.get(0));
+	}
+	
+	@Test
+	void registerTest() {
+		TournamentServerClient client = new TournamentServerClient("http://localhost:" + port);
+		
+		client.register("ipd-1","testPlayer","127.0.0.1",9090);
+		//register test player and check if the player is in there. /registered only exists in testing
+		//to make sure registration works 
+		List<?> registered =  restTemp.getForObject("http://localhost:"+port+"/registered", List.class);
+		assertNotNull(registered);
+		assertTrue(registered.contains("testPlayer"));
+		
+	}
+	
 
 
 }
